03c6fa6aa4a41b2deb4cdc0861fe2dc695904574
EVERTON DA SILVA GONCALVES <CORP\evertongoncalves@no018003.corp.campos.rj.gov.br>
2020-01-28 18:16:33 -0300

    pushing all

--- src/App.js
+++ src/App.js
@@ -1,24 +1,137 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
+
+import './global.css'
+import './App.css'
+import './sidebar.css'
+import './main.css'
+// Componente: Bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação
+// Proproedade: (atributo)
+// Estado: Informações mantidas pelo componente
 
 function App() {
+
+  const [devs, setDevs] = useState([]);
+  const [latitude, setLatitude] = useState('');
+  const [github_username, setGithubUsername] = useState('');
+  const [techs, setTechs] = useState('');
+  const [longitude, setLongitude] = useState('');
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.log(position)
+        const { latitude, longitude } = position.coords;
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      (err) => {
+        console.log(err);
+      },
+      {
+        timeout: 30000,
+      }
+    )
+  }, []);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('./devs');
+
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(e) {
+    e.preventDefault();
+    
+    const response = await api.post('/devs', {
+      github_username,
+      techs,
+      latitude,
+      longitude
+    })
+
+    setGithubUsername('');
+    setTechs('');
+
+    setDevs([...devs, response.data]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form onSubmit={handleAddDev}>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input 
+              maxLength="20" 
+              type="text" 
+              name="github_username" 
+              id="github_username" 
+              required
+              value={github_username}
+              onChange={e => setGithubUsername(e.target.value)} />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input 
+              type="text" 
+              name="techs" 
+              id="techs" 
+              required 
+              value={techs}
+              onChange={e => setTechs(e.target.value)} />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input 
+                type="text" 
+                name="latitude" 
+                id="latitude" 
+                onChange={ e => setLatitude(e.target.value)} value={latitude} 
+                required />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input 
+                type="text" 
+                name="longitude" 
+                id="longitude" 
+                onChange={e => setLongitude(e.target.value)} value={longitude} 
+                required />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+
+      <main>
+        <ul>
+          {devs.map(dev => (
+            <li key={dev._id} className="dev-item">
+              <header>
+                <img src={dev.avatar_url} alt={dev.name} />
+                <div className="user-info">
+                  <strong>{dev.name}</strong>
+                  <span>{dev.techs.join(', ')}</span>
+                </div>
+              </header>
+              <p>{dev.bio}</p>
+              <a href={`https://github.com/${github_username}`}>Acessar perfil no Github</a>
+            </li>
+          ))}
+          
+        </ul>
+      </main>
     </div>
   );
 }
